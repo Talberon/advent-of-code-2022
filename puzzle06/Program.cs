f5f9084387baf19ee0c88a76f365442823283744
Talberon <atheion@outlook.com>
2022-12-06 09:30:23 -0400

    Solved Day 06 Part 2

--- puzzle06/Program.cs
+++ puzzle06/Program.cs
@@ -1,12 +1,10 @@
-﻿// Opening the existing file for reading
+﻿using System.Text;
 
-using System.Text;
-
-const int packetMarkerSize = 4;
+const int messageMarker = 14; //4
 
 using FileStream stream = File.OpenRead("input.txt");
 
-byte[] initialSegment = new byte[packetMarkerSize];
+byte[] initialSegment = new byte[messageMarker];
 var encoding = new UTF8Encoding(true);
 
 Queue<char> buffer = new();
@@ -16,12 +14,12 @@ if (stream.Read(initialSegment, 0, initialSegment.Length) > 0)
     encoding.GetChars(initialSegment).ToList().ForEach(c => buffer.Enqueue(c));
 }
 
-int processed = packetMarkerSize;
+int processed = messageMarker;
 byte[] segment = new byte[1];
 
 while (stream.Read(segment, 0, segment.Length) > 0)
 {
-    if (buffer.Distinct().ToArray().Length == packetMarkerSize)
+    if (buffer.Distinct().ToArray().Length == messageMarker)
     {
         Console.WriteLine(
             $"Found packet start marker:\t({string.Join("", buffer)}) after ({processed}) characters processed.");
