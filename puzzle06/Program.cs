c355fb84d00ffa935c2679ff5afa9a90c8da4eb8
Talberon <atheion@outlook.com>
2022-12-06 09:38:44 -0400

    Refactoring Day 06

--- puzzle06/Program.cs
+++ puzzle06/Program.cs
@@ -1,32 +1,24 @@
 ﻿using System.Text;
 
-const int messageMarker = 14; //4
+const int markerSize = 14; //4
 
 using FileStream stream = File.OpenRead("input.txt");
-
-byte[] initialSegment = new byte[messageMarker];
 var encoding = new UTF8Encoding(true);
+byte[] segment = new byte[1];
 
 Queue<char> buffer = new();
-
-if (stream.Read(initialSegment, 0, initialSegment.Length) > 0)
-{
-    encoding.GetChars(initialSegment).ToList().ForEach(c => buffer.Enqueue(c));
-}
-
-int processed = messageMarker;
-byte[] segment = new byte[1];
+int processed = 0;
 
 while (stream.Read(segment, 0, segment.Length) > 0)
 {
-    if (buffer.Distinct().ToArray().Length == messageMarker)
+    if (buffer.Distinct().Count() == markerSize)
     {
-        Console.WriteLine(
-            $"Found packet start marker:\t({string.Join("", buffer)}) after ({processed}) characters processed.");
+        Console.WriteLine($"Found packet start marker:\t({string.Join("", buffer)}) after ({processed}) characters processed.");
         return;
     }
 
-    buffer.Dequeue();
+    if (buffer.Count == markerSize) buffer.Dequeue();
+    
     buffer.Enqueue(encoding.GetChars(segment)[0]);
     processed++;
 }
