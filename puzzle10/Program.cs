c75ac76eba8c1d7836b05c05f9bcb1898816cb20
Talberon <atheion@outlook.com>
2022-12-10 12:17:51 -0400

    Refactored Day 10

--- puzzle10/Program.cs
+++ puzzle10/Program.cs
@@ -4,56 +4,27 @@ using System.Text.RegularExpressions;
 
 var lines = new Queue<string>(File.ReadAllLines("input"));
 
-const bool debugMode = true;
-Action<string?> debugConsole = debugMode ? Console.WriteLine : (_) => { };
+List<int> signalIntervals = new() { 20, 60, 100, 140, 180, 220 };
 
-// `addx V` takes two cycles to complete. After two cycles, the X register is increased by the value V. (V can be negative.)
-// `noop` takes one cycle to complete. It has no other effect.
+int x = 1;
 
-// Find the signal strength during the 20th, 60th, 100th, 140th, 180th, and 220th cycles. What is the sum of these six signal strengths?
+//Part 1
+const int cycleLag = 2;
+(int addition, int cyclesRemaining) pending = new(0, -1);
+int signalStrengthTotal = 0;
 
-const int start = 20;
-const int interval = 40;
-const int intervalIterations = 5;
-const int intervalLag = 2;
-
-List<int> intervalsToCheck = new() { start };
-for (int i = 0; i < intervalIterations; i++)
-{
-    intervalsToCheck.Add(intervalsToCheck[^1] + interval);
-}
-
-Console.WriteLine($"Intervals to check: {string.Join(", ", intervalsToCheck)}");
-
-// How CRT Works:
-// CRT is an array of bools (representing pixels) bool[]
-// - Start at 0
-// - Scan from left to right 3 pixels wide from CURRENT REGISTER (X) as the MIDDLE
-
-// - After drawing everything, print screen, newlining every crtWidth(40) pixels
-
+//Part 2
 const int crtWidth = 40;
 const int crtHeight = 6;
 string crtScreen = "";
 
-
-int x = 1;
-(int addition, int cyclesRemaining) pending = new(0, -1);
-int signalStrengthTotal = 0;
-
-
-const int totalCycles = crtWidth * crtHeight; // Part 01: intervalsToCheck[^1];
-
-for (int cycle = 1; cycle <= totalCycles; cycle++)
+for (int cycle = 1; cycle <= crtWidth * crtHeight; cycle++)
 {
-    debugConsole($"[Cycle {cycle:000}] X = {x:000}");
-    debugConsole($"[Cycle {cycle:000}] Current CRT Row: {crtScreen}");
-
     if (pending.cyclesRemaining > 0)
     {
         pending.cyclesRemaining--;
-        UpdateSignalStrength();
-        UpdateSprite();
+        UpdateSignalStrength(cycle);
+        UpdateCrtScreen();
         continue;
     }
 
@@ -62,49 +33,44 @@ for (int cycle = 1; cycle <= totalCycles; cycle++)
         x += pending.addition;
     }
 
-
-    UpdateSignalStrength();
-    UpdateSprite();
+    UpdateSignalStrength(cycle);
+    UpdateCrtScreen();
 
     string line = lines.Count > 0 ? lines.Dequeue() : "";
-    debugConsole($"[Cycle {cycle:000}] Read Command: {line}");
 
     if (line.StartsWith("addx"))
     {
         int addition = Convert.ToInt32(line.Split(" ")[1]);
-        pending = (addition, intervalLag);
+        pending = (addition, cycleLag);
     }
 
     pending.cyclesRemaining--;
+}
 
-    debugConsole("");
+Console.WriteLine($"[Part 1] Total Signal Strength: {signalStrengthTotal}");
+Console.WriteLine($"[Part 2] Screen:\n{ScreenOutput(crtScreen)}");
 
-    //Methods
+return;
 
-    void UpdateSignalStrength()
+//Methods
+
+void UpdateSignalStrength(int cycle)
+{
+    if (!signalIntervals.Contains(cycle)) return;
+    signalStrengthTotal += x * cycle;
+}
+
+void UpdateCrtScreen()
+{
+    int column = crtScreen.Length % crtWidth;
+    if (x - 1 == column || x == column || x + 1 == column)
     {
-        if (!intervalsToCheck.Contains(cycle)) return;
-        debugConsole($"[Cycle {cycle:000}] X = {x}");
-        signalStrengthTotal += x * cycle;
+        crtScreen += "#";
     }
-
-    void UpdateSprite()
+    else
     {
-        int column = crtScreen.Length % crtWidth;
-        if (x - 1 == column || x == column || x + 1 == column)
-        {
-            crtScreen += "#";
-        }
-        else
-        {
-            crtScreen += ".";
-        }
+        crtScreen += ".";
     }
 }
 
-//Assumes input ends with several no-ops.
-Console.WriteLine($"Done! Total Signal Strength: {signalStrengthTotal}");
-Console.WriteLine($"Screen:\n{Regex.Replace(crtScreen, "(.{" + crtWidth + "})", "$1" + Environment.NewLine)}");
-
-
-// int totalCycles = lines.Select(line => line.StartsWith("addx") ? 2 : 1).Sum();
+string ScreenOutput(string input) => Regex.Replace(input, "(.{" + crtWidth + "})", "$1" + Environment.NewLine);
