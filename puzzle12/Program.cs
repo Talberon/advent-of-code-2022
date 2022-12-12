44df42022150343092b9d1d23feb310e2e1fd103
Talberon <atheion@outlook.com>
2022-12-12 16:50:06 -0400

    Finished Day 12 Part 1

--- puzzle12/Program.cs
+++ puzzle12/Program.cs
@@ -7,24 +7,20 @@
 //Can move to a destination at most one higher eg. g->h or b->c etc.
 //Can jump down eg. l->c or e->d or z->a
 
-/*
-Algorithm:
 
-Each step, log the number of legal moves
+string[] lines = File.ReadAllLines("input");
 
-If a step one higher than this one is available, take it
-If not, take the highest legal letter available
+List<List<GridCell>> grid =
+    lines.Select(
+        line => line.Select(letter => new GridCell
+        {
+            Letter = letter,
+            DistanceFromOrigin = null
+        }).ToList()
+    ).ToList();
 
-Do not repeat position (leave breadcrumb)
 
-If hitting a dead-end, reverse until you reach a fork in the road and block off the path you took
-*/
-
-string[] lines = File.ReadAllLines("test_input_2");
-
-List<List<char>> grid = lines.Select(line => line.Select(letter => letter).ToList()).ToList();
-
-Queue<Position> visited = new();
+Stack<Position> visited = new();
 
 Position currentPosition = GetPositionsOf('S').First();
 Position summit = GetPositionsOf('E').First();
@@ -32,51 +28,70 @@ Position summit = GetPositionsOf('E').First();
 Console.WriteLine($"Start: {currentPosition} | Summit: {summit}");
 PrintGrid(grid, currentPosition, summit);
 
-while (!currentPosition.Equals(summit))
+//From starting point, create a DistanceFromOrigin grid tracking the cost
+SetDistanceFromOriginAtPosition(currentPosition, 0);
+
+int currentDistance = 0;
+
+while (grid[summit.Y][summit.X].DistanceFromOrigin is null)
 {
-    int currentElevation = ElevationOf(currentPosition);
+    //New strategy
 
-    List<(Position pos, int elevation)> options = new List<Position>
+    //Beginning with start point, update DistanceFromOrigins for each legal move around current distance
+
+    List<Position> maxDistanceCells = new();
+    for (int row = 0; row < grid.Count; row++)
+    {
+        for (int col = 0; col < grid[0].Count; col++)
         {
-            currentPosition + Position.Down,
-            currentPosition + Position.Right,
-            currentPosition + Position.Up,
-            currentPosition + Position.Left,
+            if (grid[row][col].DistanceFromOrigin == currentDistance)
+            {
+                maxDistanceCells.Add(new Position { X = col, Y = row });
+            }
         }
-        .Where(pos => pos.X >= 0 && pos.Y >= 0 && pos.X < grid[0].Count && pos.Y < grid.Count)
-        .Select(position => (position, elevation: ElevationOf(position)))
-        .Where(tup => tup.elevation <= currentElevation + 1 && !visited.Contains(tup.position))
-        .OrderByDescending(tup => tup.elevation)
-        .ThenBy(tup => tup.position.DistanceFrom(summit))
-        .ToList();
-
-
-    if (options.All(tup => tup.elevation < currentElevation) || options.Count == 0)
-    {
-        grid[currentPosition.Y][currentPosition.X] = '.';
-
-        Console.WriteLine($"REVERSING TO LAST POSITION | Current: {currentPosition} -> Previous: {visited.Last()}");
-        currentElevation = ElevationOf(currentPosition);
-        currentPosition = visited.Dequeue();
-    }
-    else
-    {
-        (currentPosition, currentElevation) = options.First();
-        visited.Enqueue(currentPosition);
     }
 
+    foreach (Position position in maxDistanceCells)
+    {
+        currentPosition = position;
 
-    PrintGrid(grid, currentPosition, summit);
-    Console.WriteLine($"[{currentPosition} ({currentElevation})] Options: {string.Join(", ", options)}");
-    Console.WriteLine();
-    Thread.Sleep(300);
+        List<GridCell> legalCells = new List<Position>
+            {
+                currentPosition + Position.Down,
+                currentPosition + Position.Right,
+                currentPosition + Position.Up,
+                currentPosition + Position.Left,
+            }
+            .Where(pos => pos.X >= 0 && pos.Y >= 0 && pos.X < grid[0].Count && pos.Y < grid.Count)
+            .Select(pos => grid[pos.Y][pos.X])
+            .Where(cell =>
+                cell.Elevation - 1 <= CellAtPosition(currentPosition).Elevation && cell.DistanceFromOrigin is null)
+            .ToList();
+        
+        Console.WriteLine($"Legal cells: {string.Join(",", legalCells)}");
+
+        foreach (GridCell legalCell in legalCells)
+        {
+            legalCell.DistanceFromOrigin = currentDistance + 1;
+        }
+    }
+
+    // PrintGrid(grid, currentPosition, summit);
+    // Console.WriteLine();
+    // Thread.Sleep(500);
+    currentDistance++;
+}
+
+PrintGrid(grid, currentPosition, summit);
+Console.WriteLine($"Steps taken: {grid[summit.Y][summit.X].DistanceFromOrigin}");
+
+void SetDistanceFromOriginAtPosition(Position position, int distanceFromOrigin)
+{
+    grid[position.Y][position.X].DistanceFromOrigin = distanceFromOrigin;
 }
 
 
-Console.WriteLine($"Steps taken: {visited.Count}");
-
-
-void PrintGrid(List<List<char>> grid, Position you, Position top)
+void PrintGrid(List<List<GridCell>> grid, Position you, Position top)
 {
     Write("\t");
 
@@ -89,16 +104,21 @@ void PrintGrid(List<List<char>> grid, Position you, Position top)
 
     for (int row = 0; row < grid.Count; row++)
     {
-        List<char> line = grid[row];
+        List<GridCell> line = grid[row];
         Write($"[{row}]\t ");
 
         for (int column = 0; column < line.Count; column++)
         {
-            char letter = line[column];
+            char letter = line[column].Letter;
+            int? distanceFromOrigin = line[column].DistanceFromOrigin;
 
             var position = new Position { X = column, Y = row };
 
-            if (position.Equals(you))
+            if (distanceFromOrigin.HasValue)
+            {
+                Write(@"{=Blue}" + distanceFromOrigin + "{/} "); //({HeightOf(letter)})}
+            }
+            else if (position.Equals(you))
             {
                 Write(@"{=Red}X{/} "); //({HeightOf(letter)})
             }
@@ -127,22 +147,16 @@ void PrintGrid(List<List<char>> grid, Position you, Position top)
     }
 }
 
-char GetCharAt(Position position) => grid[position.Y][position.X];
+
+GridCell CellAtPosition(Position position)
+{
+    return grid[position.Y][position.X];
+}
 
 List<Position> GetPositionsOf(char letter) => (from line in grid
     from item in line
-    where item == letter
+    where item.Letter == letter
     select new Position { X = line.IndexOf(item), Y = grid.IndexOf(line) }).ToList();
-
-int ElevationOf(Position position) => HeightOf(GetCharAt(position));
-
-int HeightOf(char letter) => letter switch
-{
-    'S' => 1,
-    'E' => 26,
-    '.' => 99, //Visited
-    _ => letter - 'a' + 1
-};
 
 void Write(string msg)
 {
@@ -155,6 +169,26 @@ void Write(string msg)
             Console.ForegroundColor = c;
         else
             Console.Write(s);
+}
+
+class GridCell
+{
+    public int? DistanceFromOrigin { get; set; }
+
+    public char Letter { get; init; }
+
+    public int Elevation => Letter switch
+    {
+        'S' => 1,
+        'E' => 26,
+        '.' => 99, //Visited
+        _ => Letter - 'a' + 1
+    };
+
+    public override string ToString()
+    {
+        return $"[GridCell: (DistanceFromOrigin: {DistanceFromOrigin}, Letter: {Letter}, Elevation: {Elevation})]";
+    }
 }
 
 internal readonly struct Position
@@ -186,18 +220,6 @@ internal readonly struct Position
         int straightSteps = Math.Max(deltaX, deltaY) - diagonalSteps;
 
         return Convert.ToInt32(Math.Sqrt(2) * diagonalSteps + straightSteps);
-    }
-
-    private Position MoveToward(Position other)
-    {
-        Position temp = this;
-
-        if (temp.IsAbove(other)) temp += Down;
-        if (temp.IsUnder(other)) temp += Up;
-        if (temp.IsLeftOf(other)) temp += Right;
-        if (temp.IsRightOf(other)) temp += Left;
-
-        return temp;
     }
 
     private bool IsAbove(Position other) => Y > other.Y;
