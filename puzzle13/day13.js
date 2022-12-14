c51a0cd5be544b6c1183b7ea76d9a5ac725c6c2b
Talberon <atheion@outlook.com>
2022-12-13 21:23:50 -0400

    Day 13 Refactoring

--- puzzle13/day13.js
+++ puzzle13/day13.js
@@ -1,136 +1,87 @@
 // Day 13: Distress Signal
-
-// Int vs Int: If left side is higher, input is out of order
-// [] vs []: If left side has more items, input is out of order. Else, iterate through ints. If left side is higher, input is out of order.
-// [] vs Int: Compare int to the first item in []. If left side is higher, input is out of order.
-
 const fs = require('fs');
 
 fs.readFile('./input', (error, data) => {
   if (error) throw error;
   const input = data.toString().split(/\r?\n/).reverse();
 
-  // Part01(input);
-  Part02(input);
+  const answer01 = Part01(input);
+  console.log(`Part 1: `, answer01);
+
+  const dividerPackets = ["[[2]]", "[[6]]"];
+  const answer02 = Part02(input, dividerPackets);
+  console.log(`Part 2: `, answer02);
 });
 
-function Part02(input) {
-  const dividerPackets = ["[[2]]", "[[6]]"];
+function Part02(input, dividerPackets) {
   const allPackets = [...input, ...dividerPackets]
     .filter(line => line.length > 0)
     .map(line => JSON.parse(line))
-    .sort((left, right) => {
-      const correct = isInCorrectOrder(left, right);
-      return correct;
-    })
-    .reverse();
+    .sort((a, b) => isInCorrectOrder(b, a));
 
-  let dividerTwoIndex;
-  let dividerSixIndex;
+  let dividerPacketProduct = 1;
 
-  for (let i = 1; i <= allPackets.length; i++) {
-    const current = JSON.stringify(allPackets[i-1]);
-    console.log(`[${i}]\t${current}`);
-    if (current === "[[2]]") dividerTwoIndex = i;
-    if (current === "[[6]]") dividerSixIndex = i;
+  for (let i = 0; i < allPackets.length; i++) {
+    const current = JSON.stringify(allPackets[i]);
+    console.log(`[${i + 1}]\t${current}`);
+    if (dividerPackets.includes(current)) {
+      dividerPacketProduct *= (i + 1);
+    }
   }
 
-  console.log(`Two Index: ${dividerTwoIndex} * Six Index: ${dividerSixIndex} = ${dividerTwoIndex * dividerSixIndex}`);
-
+  return dividerPacketProduct;
 }
 
-
 function Part01(input) {
+  const lines = [...input];
+
   let pairIndex = 1;
   let runningSum = 0;
 
-  while (input.length > 0) {
+  while (lines.length > 0) {
 
-    const left = JSON.parse(input.pop());
-    const right = JSON.parse(input.pop());
+    const left = JSON.parse(lines.pop());
+    const right = JSON.parse(lines.pop());
+    if (lines.length > 0) lines.pop(); //Discard empty row
 
-    if (input.length > 0)
-      input.pop(); //Discard empty row
-
-    console.log(`== Pair ${pairIndex} ==`);
-    console.log(`Compare ${JSON.stringify(left)}\n     vs ${JSON.stringify(right)}\nSTART`);
     const isCorrect = isInCorrectOrder(left, right);
     if (isCorrect > 0)
       runningSum += pairIndex;
-    console.log(`\n[Pair ${pairIndex}] Is in correct order?`, isCorrect, "Answer:", runningSum);
 
     pairIndex++;
-    console.log("");
   }
+
+  return runningSum;
 }
 
 function isInCorrectOrder(left, right) {
   for (let i = 0; i < left.length; i++) {
-    // console.log(`Compare ${JSON.stringify(left[i])} vs ${JSON.stringify(right[i])}`);
 
-    if (right[i] == undefined) {
-      // console.log(`Right side ran out of items, so WRONG`);
-      return -1;
-    }
+    if (right[i] == undefined) return -1;
 
     if (typeof left[i] === 'number' && typeof right[i] == 'number') {
-      if (left[i] < right[i]) {
-        // console.log(`Left side is smaller, so RIGHT`);
-        return 1;
-      }
-      if (left[i] > right[i]) {
-        // console.log(`Right side is smaller, so WRONG`);
-        return -1;
-      }
+      if (left[i] < right[i]) return 1;
+      if (left[i] > right[i]) return -1;
       continue;
     }
     if (left[i] instanceof Array && right[i] instanceof Array) {
       const result = isInCorrectOrder(left[i], right[i]);
-      if (result > 0) {
-        // console.log(`Propogating TRUE answer up`);
-        return 1;
-      }
-      if (result < 0) {
-        // console.log(`Propogating FALSE answer up`);
-        return -1;
-      }
-      // console.log(`Continuing...`);
+      if (result > 0) return 1;
+      if (result < 0) return -1;
     }
     if (typeof left[i] == 'number' && right[i] instanceof Array) {
-      // console.log(`Mixed types; convert left to [${left[i]}] and retry comparison`);
       const result = isInCorrectOrder([left[i]], right[i]);
-      if (result > 0) {
-        // console.log(`Propogating TRUE answer up`);
-        return 1;
-      }
-      if (result < 0) {
-        // console.log(`Propogating FALSE answer up`);
-        return -1;
-      }
-      // console.log(`Continuing...`);
+      if (result > 0) return 1;
+      if (result < 0) return -1;
     }
     if (left[i] instanceof Array && typeof right[i] == 'number') {
-      // console.log(`Mixed types; convert right to [${right[i]}] and retry comparison`);
       const result = isInCorrectOrder(left[i], [right[i]]);
-      if (result > 0) {
-        // console.log(`Propogating TRUE answer up`);
-        return 1;
-      }
-      if (result < 0) {
-        // console.log(`Propogating FALSE answer up`);
-        return -1;
-      }
-      // console.log(`Continuing...`);
+      if (result > 0) return 1;
+      if (result < 0) return -1;
     }
   }
+  
+  if (left.length == right.length) return 0;
 
-  if (left.length == right.length) {
-    // console.log(`Left and right are same size, so CONTINUE`);
-    return 0;
-  }
-
-  if (left.length < right.length) {
-    // console.log(`Left side ran out of items, so RIGHT`);
-    return 1;
-  }
+  if (left.length < right.length) return 1;
 }
